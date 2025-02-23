79709baae52d1b42b314336b1646d99d821b1d5a
natellu <t.le0nh4rdt@gmail.com>
2025-02-23 16:44:06 +0100

    check env vars for default ip and port of remote monitor mod

--- src/constants/defaultSettingsData.ts
+++ src/constants/defaultSettingsData.ts
@@ -1,7 +1,7 @@
 import type { SettingsData } from "../types/settingsData";
 
 export const defaultSettingsData: SettingsData = {
-  ip: "localhost",
-  port: "8080",
+  ip: process.env.DEFAULT_IP || "localhost",
+  port: process.env.DEFAULT_PORT || "8080",
   interval: 10000,
 };
